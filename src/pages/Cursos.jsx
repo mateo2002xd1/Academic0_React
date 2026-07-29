f35b9594b24b2119831217d4741a021a6e6567b0
mateo2002xd1 <mateo2002xd1@gmail.com>
2026-07-29 13:31:48 -0500

    Ajustes en la búsqueda y paginación de cursos

--- src/pages/Cursos.jsx
+++ src/pages/Cursos.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useDebugValue, useEffect, useState } from "react";
 import CursoCard from "../components/CursoCard";
 import { Link } from "react-router-dom";
 import api from "../service/api";
@@ -7,8 +7,35 @@ import { AuthContext } from "../context/AuthContext";
 
 function Cursos(){
     const [curso, setCurso] = useState([]);
+    const [pagina, setPagina] = useState(0);
+    const [totalPaginas, setTotalPaginas] = useState(0);
+    const [nombreCurso, setNombreCurso] = useState("");
     const { rol } = useContext(AuthContext);
-    useEffect(() => {api.get("/curso").then((res) => setCurso(res.data.content)).catch((e) => console.log(e.data))}, []);
+    const cargarCursos = (paginaActual, nombre) => {
+        api.get(`/curso?page=${paginaActual}&size=10&nombre=${nombre}`)
+            .then((res) => {
+                setCurso(res.data.content);
+                setPagina(res.data.number);
+                setTotalPaginas(res.data.totalPages);
+            })
+            .catch((e) => console.log(e));
+    };
+
+    const cargarCursosPorNombre = (e, nombre) => {
+        e.preventDefault();
+
+        cargarCursos(0, nombre);
+    };
+
+    useEffect(()=>{cargarCursos(0, nombreCurso);},[nombreCurso]);
+    
+    const paginaSiguiente = () => {
+        cargarCursos(pagina + 1, nombreCurso);
+    };
+
+    const paginaAnterior = () => {
+        cargarCursos(pagina - 1, nombreCurso);
+    };
 
     return (
         <main className="contenido">
@@ -18,12 +45,20 @@ function Cursos(){
             </h1>
             <h2>Total de cursos: {curso.length}</h2>
 
+            <form onSubmit={(e) => cargarCursosPorNombre(e, nombreCurso)}>
+                <label>Buscar: </label>
+                <input type="text" value={nombreCurso} onChange={(e) => setNombreCurso(e.target.value)}/>
+                <button>Buscar</button>
+            </form>
             {
                 curso.map((curso) => {
-                    console.log(curso);
                     return <CursoCard name={curso.nombre} desc={curso.descripcion} activo={curso.activo} id={curso.id} key={curso.id} fechacreacion={curso.fechacreacion} mostrarBotonDescripcion={true}/>
                 })
             }
+
+            <button onClick={() => paginaAnterior()} disabled={pagina === 0}>Anterior</button>
+            <button onClick={() => paginaSiguiente()} disabled={pagina + 1 >= totalPaginas}>Siguiente</button>
+            pagina {pagina + 1} de {totalPaginas}
         </main>
     );
 }
